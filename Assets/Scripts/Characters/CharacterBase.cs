9d762da66347f9deadfb0d465c7727a974c6c86b
Aoki1104 <36557366+Aoki1104@users.noreply.github.com>
2019-06-20 01:58:00 +0900

    ver  1.2 ----個人ブランチへの初コミット---------- --新要素 ---プログラム--- 　--キャラクターの基本動作（止まる・歩く）の処理を追加            追加クラス:CharacterBase 　　　　　　　 GoleamControll 　　　　　　　  FairyControll 　　　　　　　 GroundWalk 　　　　　　　 Flying
    　　 追加インターフェイス: iWalk
    
    --素材---
    　--無料アセットのゴーレムを追加
    　--無料アセットのドラゴン追加
    
    ー変更点
    「Character」フォルダを「Characters」フォルダに変更
    ※前回から何か削除した場合も変更点の欄に書いてね

---
+++ Assets/Scripts/Characters/CharacterBase.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// キャラクターの基底クラス
+/// </summary>
+public class CharacterBase : MonoBehaviour
+{
+    //基本動作
+    private enum BasicState {
+        Idle,
+        Walk
+    }
+
+    BasicState basic_state =BasicState.Idle;
+
+    private Vector3 destination_point;    //目的地
+    private Vector3 diff;
+    private Rigidbody rigid;
+    private iWalk iwalk;
+    private float wait_time;      //Idleの時間
+    private float max_wait_time = 2.5f;
+    private float idle_time;
+    [SerializeField] private float walk_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iwalk = this.gameObject.GetComponent<iWalk>();
+        rigid = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StateControll();
+    }
+
+    //ステート(状態)を管理する関数
+    void StateControll()
+    {
+
+
+        switch (basic_state)
+        {
+            //待機
+            case BasicState.Idle:
+
+                    idle_time += Time.deltaTime;
+                if (idle_time > wait_time)
+                {
+                    idle_time = 0.0f;
+                    SetState(BasicState.Walk);
+                }
+
+                break;
+
+            //歩く
+            case BasicState.Walk:
+                //進行方向へ向く
+               
+                if (diff.magnitude > 0.01f)
+                    transform.rotation = Quaternion.LookRotation(diff);
+                                                                      
+
+                
+
+                transform.Translate(Vector3.forward * walk_speed * Time.deltaTime);
+
+                if ((this.transform.position - destination_point).sqrMagnitude < 1)
+                    SetState(BasicState.Idle);
+                break;
+
+            default:
+                SetState(BasicState.Idle);
+                break;
+         }
+    }
+
+    //ステート(状態)の初期処理
+    void SetState(BasicState _state)
+    {
+        basic_state = _state;
+
+        switch (basic_state)
+        {
+            //待機
+            case BasicState.Idle:
+
+                idle_time = 0.0f;
+                wait_time = Random.Range(0.0f, max_wait_time);
+                rigid.velocity = Vector3.zero;
+                break;
+
+            //歩く
+            case BasicState.Walk:
+                iwalk.GetCharaPosition(this.transform.position);
+                destination_point = iwalk.SetDestination();
+                diff = destination_point - this.transform.position ;
+                Debug.Log("destination:" + destination_point);
+               // this.transform.LookAt(destination_point);
+                break;
+
+            default:
+                SetState(BasicState.Idle);
+                break;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Characters/Flying.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 飛行するクラス
+/// </summary>
+public class Flying : MonoBehaviour,iWalk
+{
+    private Vector3 character_position;
+    private float max_position = 10;
+    private float min_position = -10;
+
+    private Vector3 ground_position;
+    private float max_y;
+    private float min_y;
+
+    private void Start()
+    {
+        ground_position = GameObject.FindGameObjectWithTag("Ground").transform.position;
+        max_y = ground_position.y + max_position;
+        min_y = ground_position.y + 1;
+    }
+
+    public void GetCharaPosition(Vector3 chara_position)
+    {
+        character_position = chara_position;
+    }
+
+    public Vector3 SetDestination()
+    {
+        character_position.x += Random.Range(min_position, max_position);
+        character_position.z += Random.Range(min_position, max_position);
+        character_position.y += Random.Range(min_y, max_y);
+
+        return character_position;
+    }
+
+    public void WalkEnd()
+    {
+
+    }
+}
