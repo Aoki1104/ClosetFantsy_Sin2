8342b1bec9351fa384e5e2cd18a32342434d2c73
aokitatsuya <taoki0108@gmail.com>
2019-06-20 18:46:06 +0900

    --ver2.0 --サブディスプレイの画面調整をいろいろやったバージョン

---
+++ Assets/Scripts/Game System/MultiDisplay.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MultiDisplay : MonoBehaviour
+{
+    void Start()
+    {
+        int maxDisplayCount = 2;
+        for (int i = 0; i < maxDisplayCount && i < Display.displays.Length; i++)
+        {
+            if(i != 1) 
+            Display.displays[i].Activate();
+            else if(i == 1)
+             Display.displays[i].Activate(1024,600,60);
+        }
+    }
+
+    void OnGUI()
+    {
+        var mousePos = Display.RelativeMouseAt(Input.mousePosition);
+        GUILayout.Label("mouse position:" + ((Vector2)mousePos).ToString());
+        GUILayout.Label("display id:" + mousePos.z);
+    }
+
+} // class GameController
