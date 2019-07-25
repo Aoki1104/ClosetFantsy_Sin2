1f9c769cde9cf76d5b686cf024adc53b9c9c5ca5
aokitatsuya <taoki0108@gmail.com>
2019-07-25 17:59:36 +0900

    commit pumpkincontroller pumpukinlight をHallowenのScriptsへ移動

---
+++ Assets/Project/Programa/Halloween/Scripts/PumpkinControll.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class PumpkinControll : MonoBehaviour
+{
+    [SerializeField]
+    float jumpPower = 300.0f;       //ジャンプ力ぅ
+    [SerializeField]
+    float waittime = 500.0f;
+    
+    private Rigidbody _rigit;
+    private Vector3 pos;
+    private CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+            Rigidbody rb = this.GetComponent<Rigidbody>();  // rigidbodyを取得
+            Vector3 force = new Vector3(0.0f, jumpPower, 0.0f);    // 力を設定
+            rb.AddForce(force);  // 力を加える
+        StartCoroutine(waiter());
+
+    }
+
+    IEnumerator waiter()
+    {
+        Debug.Log("ウェイト"); 
+        yield return new WaitForSeconds(waittime);
+    }
+}
