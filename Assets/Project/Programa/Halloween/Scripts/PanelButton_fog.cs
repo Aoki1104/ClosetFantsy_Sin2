69345bab2808305db1861fac32ec1e705947d67e
xre1x <minecraft.pe3090@gmail.com>
2019-07-27 18:02:40 +0900

    変更対象：world_Hallo 　　内容：霧の調整（fog）UIの押下により霧の減少、出現を実装。

---
+++ Assets/Project/Programa/Halloween/Scripts/PanelButton_fog.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.ImageEffects
+{
+    public class PanelButton_fog : MonoBehaviour
+    {
+        [SerializeField] private GlobalFog script;
+        private bool Button_fog;
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float startfog = script.startDistance;
+            Debug.Log("StartDistanceは" + startfog);
+
+        }
+        public void OnClick()
+        {
+
+            Debug.Log("押された!");  // ログを出力
+            if (Button_fog == false)
+            {
+                //ボタンをオンにする
+                Button_fog = true;
+                    script.Add1TostartDistance();
+            }
+                
+                else
+                {
+                    //ボタンをオフにする
+                    Button_fog = false;
+                }
+                
+                print(":fog:" + Button_fog);
+            }
+        }
+    }
